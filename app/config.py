d032542bb141f5cc665e057c631baba78f0982d7
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2022-10-06 14:18:46 +0200

    config: always use REDIS_URL from env

--- app/config.py
+++ app/config.py
@@ -434,7 +434,6 @@ class Development(Config):
     NOTIFY_EMAIL_DOMAIN = "notify.tools"
 
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://localhost/notification_api')
-    REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 
     ANTIVIRUS_ENABLED = os.getenv('ANTIVIRUS_ENABLED') == '1'
 
